d1e986222e935bc8338b8791ac04fba95ccf0c1f
Daniel P. Castro <daniel.pcastro11@gmail.com>
2023-02-17 19:33:52 -0300

    sketch of the stars difficulty, buttons cancelar and adicionar

--- lib/screens/create_task_screen.dart
+++ lib/screens/create_task_screen.dart
@@ -8,6 +8,11 @@ class CreateTask extends StatefulWidget {
 }
 
 class _CreateTaskState extends State<CreateTask> {
+  int star = 1;
+  TextEditingController nameController = TextEditingController();
+
+  //TODO Colocar a row dentro em um container, parecido com o TextFormField, com a labelText 'Dificuldade'
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +24,84 @@ class _CreateTaskState extends State<CreateTask> {
           color: Colors.black12,
           width: 375,
           height: 400,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: "Nome da Tarefa",
+                  fillColor: Color(0xFFF2F2F2), // Pale Gray
+                  filled: true,
+                ),
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    child: Icon(Icons.star),
+                    onPressed: () {
+                      setState(() {
+                        star = 1;
+                      });
+                    },
+                  ),
+                  TextButton(
+                    child: star > 1 ? Icon(Icons.star) : Icon(Icons.star_border),
+                    onPressed: () {
+                      setState(() {
+                        star = 2;
+                      });
+                    },
+                  ),
+                  TextButton(
+                    child: star > 2 ? Icon(Icons.star) : Icon(Icons.star_border),
+                    onPressed: () {
+                      setState(() {
+                        star = 3;
+                      });
+                    },
+                  ),
+                  TextButton(
+                    child: star > 3 ? Icon(Icons.star) : Icon(Icons.star_border),
+                    onPressed: () {
+                      setState(() {
+                        star = 4;
+                      });
+                    },
+                  ),
+                  TextButton(
+                    child: star > 4 ? Icon(Icons.star) : Icon(Icons.star_border),
+                    onPressed: () {
+                      setState(() {
+                        star = 5;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.blue, //Text Color
+                      backgroundColor: const Color(0xFFF2F2F2), //Button Color
+                    ),
+                    onPressed: () {},
+                    child: const Text("Cancelar"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print(nameController.text);
+                    },
+                    child: const Text("Adicionar"),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
-      );
+    );
   }
 }
