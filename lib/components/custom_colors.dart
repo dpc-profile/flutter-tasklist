2c65e7f24380ae9906f3030c7a19dca79b062515
Daniel P. Castro <daniel.pcastro11@gmail.com>
2023-02-18 12:08:09 -0300

    class with used colors in the project

---
+++ lib/components/custom_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomColors{
+
+  static Color paleGrey = const Color(0xFFF2F2F2);
+  static Color lightBlueGrey = const Color(0xFFD0DDED);
+
+
+  CustomColors();
+
+}
