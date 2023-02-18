7edd0f0f37b486e10c52129a72f3bef587ab378a
Daniel P. Castro <daniel.pcastro11@gmail.com>
2023-02-18 14:53:25 -0300

    Widget with difficulty done

--- lib/screens/create_task_screen.dart
+++ lib/screens/create_task_screen.dart
@@ -1,3 +1,4 @@
+import 'package:first_project_alura/components/custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class CreateTask extends StatefulWidget {
@@ -9,19 +10,20 @@ class CreateTask extends StatefulWidget {
 
 class _CreateTaskState extends State<CreateTask> {
   int star = 1;
-  TextEditingController nameController = TextEditingController();
+  Color paleGrey = CustomColors.paleGrey;
+  Color lightBlueGrey = CustomColors.lightBlueGrey;
 
-  //TODO Colocar a row dentro em um container, parecido com o TextFormField, com a labelText 'Dificuldade'
+  TextEditingController nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: const Color(0xFFD0DDED), // Light Blue Grey,
+      backgroundColor: lightBlueGrey,
       appBar: AppBar(title: const Text("Criar Tarefa")),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Container(
-          color: Colors.black12,
+        child: SizedBox(
           width: 375,
           height: 400,
           child: Column(
@@ -29,55 +31,96 @@ class _CreateTaskState extends State<CreateTask> {
             children: [
               TextFormField(
                 controller: nameController,
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                   labelText: "Nome da Tarefa",
-                  fillColor: Color(0xFFF2F2F2), // Pale Gray
+                  fillColor: paleGrey, // Pale Gray
                   filled: true,
                 ),
               ),
-              Row(
-                children: [
-                  TextButton(
-                    child: Icon(Icons.star),
-                    onPressed: () {
-                      setState(() {
-                        star = 1;
-                      });
-                    },
-                  ),
-                  TextButton(
-                    child: star > 1 ? Icon(Icons.star) : Icon(Icons.star_border),
-                    onPressed: () {
-                      setState(() {
-                        star = 2;
-                      });
-                    },
-                  ),
-                  TextButton(
-                    child: star > 2 ? Icon(Icons.star) : Icon(Icons.star_border),
-                    onPressed: () {
-                      setState(() {
-                        star = 3;
-                      });
-                    },
-                  ),
-                  TextButton(
-                    child: star > 3 ? Icon(Icons.star) : Icon(Icons.star_border),
-                    onPressed: () {
-                      setState(() {
-                        star = 4;
-                      });
-                    },
-                  ),
-                  TextButton(
-                    child: star > 4 ? Icon(Icons.star) : Icon(Icons.star_border),
-                    onPressed: () {
-                      setState(() {
-                        star = 5;
-                      });
-                    },
-                  ),
-                ],
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: paleGrey,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                      child: Text(
+                        "Dificuldade",
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 40,
+                          child: TextButton(
+                            child: const Icon(Icons.star),
+                            onPressed: () {
+                              setState(() {
+                                star = 1;
+                              });
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                          child: TextButton(
+                            child: star > 1
+                                ? const Icon(Icons.star)
+                                : const Icon(Icons.star_border),
+                            onPressed: () {
+                              setState(() {
+                                star = 2;
+                              });
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                          child: TextButton(
+                            child: star > 2
+                                ? const Icon(Icons.star)
+                                : const Icon(Icons.star_border),
+                            onPressed: () {
+                              setState(() {
+                                star = 3;
+                              });
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                          child: TextButton(
+                            child: star > 3
+                                ? const Icon(Icons.star)
+                                : const Icon(Icons.star_border),
+                            onPressed: () {
+                              setState(() {
+                                star = 4;
+                              });
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                          child: TextButton(
+                            child: star > 4
+                                ? const Icon(Icons.star)
+                                : const Icon(Icons.star_border),
+                            onPressed: () {
+                              setState(() {
+                                star = 5;
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -85,7 +128,7 @@ class _CreateTaskState extends State<CreateTask> {
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       foregroundColor: Colors.blue, //Text Color
-                      backgroundColor: const Color(0xFFF2F2F2), //Button Color
+                      backgroundColor: paleGrey, //Button Color
                     ),
                     onPressed: () {},
                     child: const Text("Cancelar"),
