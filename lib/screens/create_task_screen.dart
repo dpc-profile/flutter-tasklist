6e166a43c40250e5c2fcfc3ed3bd3ac9e4363ff9
Daniel P. Castro <daniel.pcastro11@gmail.com>
2023-02-27 10:53:49 -0300

    Added SnackBar

--- lib/screens/create_task_screen.dart
+++ lib/screens/create_task_screen.dart
@@ -13,164 +13,186 @@ class _CreateTaskState extends State<CreateTask> {
   Color paleGrey = CustomColors.paleGrey;
   Color lightBlueGrey = CustomColors.lightBlueGrey;
 
+  final _formKey = GlobalKey<FormState>();
+
   TextEditingController nameController = TextEditingController();
   TextEditingController imageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: lightBlueGrey,
-      appBar: AppBar(title: const Text("Criar Tarefa")),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: SizedBox(
-            width: 375,
-            height: 450,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextFormField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                    labelText: "Nome da Tarefa",
-                    fillColor: paleGrey, // Pale Gray
-                    filled: true,
-                  ),
-                ), //Titulo
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: paleGrey,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-                        child: Text(
-                          "Dificuldade",
-                          style: TextStyle(color: Colors.blue),
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        backgroundColor: lightBlueGrey,
+        appBar: AppBar(title: const Text("Criar Tarefa")),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: SizedBox(
+              width: 375,
+              height: 450,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextFormField(
+                    validator: (value) {
+                      // Verifica se e nulo e vazio
+                      if (value!.isEmpty) {
+                        return "Insira o nome da tarefa";
+                      }
+                      return null;
+                    },
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      labelText: "Nome da Tarefa",
+                      fillColor: paleGrey, // Pale Gray
+                      filled: true,
+                    ),
+                  ), //NomeTarefa
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      color: paleGrey,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                          child: Text(
+                            "Dificuldade",
+                            style: TextStyle(color: Colors.blue),
+                          ),
                         ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 40,
+                              child: TextButton(
+                                child: const Icon(Icons.star),
+                                onPressed: () {
+                                  setState(() {
+                                    star = 1;
+                                  });
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 40,
+                              child: TextButton(
+                                child: star > 1
+                                    ? const Icon(Icons.star)
+                                    : const Icon(Icons.star_border),
+                                onPressed: () {
+                                  setState(() {
+                                    star = 2;
+                                  });
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 40,
+                              child: TextButton(
+                                child: star > 2
+                                    ? const Icon(Icons.star)
+                                    : const Icon(Icons.star_border),
+                                onPressed: () {
+                                  setState(() {
+                                    star = 3;
+                                  });
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 40,
+                              child: TextButton(
+                                child: star > 3
+                                    ? const Icon(Icons.star)
+                                    : const Icon(Icons.star_border),
+                                onPressed: () {
+                                  setState(() {
+                                    star = 4;
+                                  });
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              width: 40,
+                              child: TextButton(
+                                child: star > 4
+                                    ? const Icon(Icons.star)
+                                    : const Icon(Icons.star_border),
+                                onPressed: () {
+                                  setState(() {
+                                    star = 5;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ), //Dificuldade
+                  TextFormField(
+                    onChanged: (text) {
+                      setState(() {});
+                    },
+                    keyboardType: TextInputType.url,
+                    controller: imageController,
+                    decoration: InputDecoration(
+                      labelText: "Imagem",
+                      fillColor: paleGrey, // Pale Gray
+                      filled: true,
+                    ),
+                  ), //Url Imagem
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        imageController.text,
+                        errorBuilder: ((BuildContext context, Object error,
+                                StackTrace? stackTrace) =>
+                            Image.asset("assets/img/noPhoto.png")),
                       ),
-                      Row(
-                        children: [
-                          SizedBox(
-                            width: 40,
-                            child: TextButton(
-                              child: const Icon(Icons.star),
-                              onPressed: () {
-                                setState(() {
-                                  star = 1;
-                                });
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            width: 40,
-                            child: TextButton(
-                              child: star > 1
-                                  ? const Icon(Icons.star)
-                                  : const Icon(Icons.star_border),
-                              onPressed: () {
-                                setState(() {
-                                  star = 2;
-                                });
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            width: 40,
-                            child: TextButton(
-                              child: star > 2
-                                  ? const Icon(Icons.star)
-                                  : const Icon(Icons.star_border),
-                              onPressed: () {
-                                setState(() {
-                                  star = 3;
-                                });
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            width: 40,
-                            child: TextButton(
-                              child: star > 3
-                                  ? const Icon(Icons.star)
-                                  : const Icon(Icons.star_border),
-                              onPressed: () {
-                                setState(() {
-                                  star = 4;
-                                });
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            width: 40,
-                            child: TextButton(
-                              child: star > 4
-                                  ? const Icon(Icons.star)
-                                  : const Icon(Icons.star_border),
-                              onPressed: () {
-                                setState(() {
-                                  star = 5;
-                                });
-                              },
-                            ),
-                          ),
-                        ],
+                    ),
+                  ), //Icone Imagem
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.blue, //Text Color
+                          backgroundColor: paleGrey, //Button Color
+                        ),
+                        onPressed: () {},
+                        child: const Text("Cancelar"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Salvando Tarefa"),
+                              ),
+                            );
+
+                            print(nameController.text);
+                            print(star);
+                            print(imageController.text);
+                          }
+                        },
+                        child: const Text("Adicionar"),
                       ),
                     ],
-                  ),
-                ), //Dificuldade
-                TextFormField(
-                  onChanged: (text) {
-                    setState(() {});
-                  },
-                  keyboardType: TextInputType.url,
-                  controller: imageController,
-                  decoration: InputDecoration(
-                    labelText: "Imagem",
-                    fillColor: paleGrey, // Pale Gray
-                    filled: true,
-                  ),
-                ), //Url Imagem
-                Container(
-                  height: 100,
-                  width: 72,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      imageController.text,
-                      errorBuilder: ((BuildContext context, Object error,
-                              StackTrace? stackTrace) =>
-                          Image.asset("assets/img/noPhoto.png")),
-                    ),
-                  ),
-                ), //Icone Imagem
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.blue, //Text Color
-                        backgroundColor: paleGrey, //Button Color
-                      ),
-                      onPressed: () {},
-                      child: const Text("Cancelar"),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        print(nameController.text);
-                      },
-                      child: const Text("Adicionar"),
-                    ),
-                  ],
-                ) // Botao Cancelar e Adicionar
-              ],
+                  ) // Botao Cancelar e Adicionar
+                ],
+              ),
             ),
           ),
         ),
